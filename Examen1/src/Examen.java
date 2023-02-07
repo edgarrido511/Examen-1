737d686080966aec304cbc50af96e62f5a7c9be3
edgarrido511 <122307484+edgarrido511@users.noreply.github.com>
2023-02-07 11:09:23 -0600

    Add files via upload

---
+++ Examen1/src/Examen.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		Examen a = new Examen();
+		//a.fizzBuzz();
+		//a.primeraMayuscula();
+		//a.invertirCadena();
+		//a.vocalesMayuscula();
+		a.palindromo();
+	}
+	
+	public static void fizzBuzz() {
+		for(int i = 1;i<=100;i++ ) {
+			if(i%3==0) {
+				System.out.println("Fizz");
+				continue;
+			}
+			if(i%5==0) {
+				System.out.println("Buzz");
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+	public static void primeraMayuscula(){
+		String frase = "anita lava la tina";
+		String mayuscula = "";
+		String newMayuscula;
+		String[] sep = frase.split("");
+		for(int i = 0;i<sep.length;i++) {
+			mayuscula = sep[0];
+			newMayuscula = mayuscula.toUpperCase();
+			sep[0] = newMayuscula;
+			System.out.print(sep[i]);
+		}
+	}
+	public static void invertirCadena() {
+		String frase = "anita lava la tina";
+		String[] sep = frase.split("");
+		for(int i = sep.length-1;i>=0;i--) {
+			System.out.print(sep[i]);
+		}
+	}
+	public static void vocalesMayuscula() {
+		String frase = "Hasta luego cocodrilo";
+		String mayuscula = "";
+		String newMayuscula;
+		String letras = "";
+		String[] sep = frase.split("");
+		for(int i = 0;i<sep.length;i++) {
+			if(sep[i].contains("a")){
+				mayuscula = sep[i];
+				newMayuscula = mayuscula.toUpperCase();
+				sep[i] = newMayuscula;
+			}
+			if(sep[i].contains("e")){
+				mayuscula = sep[i];
+				newMayuscula = mayuscula.toUpperCase();
+				sep[i] = newMayuscula;
+			}
+			if(sep[i].contains("i")){
+				mayuscula = sep[i];
+				newMayuscula = mayuscula.toUpperCase();
+				sep[i] = newMayuscula;
+			}
+			if(sep[i].contains("o")){
+				mayuscula = sep[i];
+				newMayuscula = mayuscula.toUpperCase();
+				sep[i] = newMayuscula;
+			}
+			if(sep[i].contains("u")){
+				mayuscula = sep[i];
+				newMayuscula = mayuscula.toUpperCase();
+				sep[i] = newMayuscula;
+			}
+			System.out.print(sep[i]);
+		}
+	}
+	public static void palindromo() {
+		String frase = "anita lava la tina";
+		frase = frase.toLowerCase();
+		String fraseJunta = frase.replace(" ","");
+		String nuevaFrase = "";
+		String[] sep = fraseJunta.split("");
+		for(int i = sep.length-1;i>=0;i--) {
+			nuevaFrase += sep[i];
+		}
+		boolean resultado = fraseJunta.equals(nuevaFrase);
+		System.out.println(resultado);
+	}
+}
